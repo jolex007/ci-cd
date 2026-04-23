9adcb0f67dece31b8adbe17a30c8a8fbef8bcb6b
Alexey <zertsalov.av@phystech.edu>
2026-04-23 12:28:52 +0000

    Fixed tests

--- c-app/test_calculator.cpp
+++ c-app/test_calculator.cpp
@@ -2,13 +2,13 @@
 #include "calculator.h"
 
 TEST(CalcTest, Add) {
-    EXCEPT_EQ(add(2, 3), 5);
+    EXPECT_EQ(add(2, 3), 5);
 }
 
 TEST(CalcTest, DevideNormal) {
-    EXCEPT_EQ(devide(6, 3), 2);
+    EXPECT_EQ(devide(6, 3), 2);
 }
 
 TEST(CalcTest, DevideByZero) {
-    EXCEPT_THROW(devide(10, 0), std::invalid_argument);
+    EXPECT_THROW(devide(10, 0), std::invalid_argument);
 }
